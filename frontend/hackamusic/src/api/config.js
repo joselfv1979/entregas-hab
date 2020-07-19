03f3bb53dac2ce689d8a1ac58edaa24c3fdaebdf
jose <jose@gmail.com>
2020-07-19 20:27:58 +0200

    frontend first delivery added

--- frontend/hackamusic/src/api/config.js
+++
@@ -1,7 +0,0 @@
-// CONFIGURACIÓN DE LA KEY PARA ACCEDER A LA API
-export default {
-    appName: 'hackamusic',
-    apiKey: '1e4a092120cd33d49ee164827a675868',
-    secret: '8e1b44274bb25c25544e82a540b62416',
-    registeredTo: 'joselfv'
-}
